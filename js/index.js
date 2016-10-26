d5ca1e8710e150fc79e9981e712e8a6b655effc0
lujiaolancode <342240955@qq.com>
2016-10-26 21:04:58 +0800

    首页第一次不完善更新

--- js/index.js
+++ js/index.js
@@ -1,4 +1,35 @@
 /**
  * Created by lujiaolan on 16/10/21.
  */
+$(function () {
+    $.ajax({
+        url: "json/content.json",
+        success: function (data) {
+            // console.log(data);
+            $.each(data.news, function (i,val) {
+                var $newsDiv = $(template("newsTemp",val));
+                $newsDiv.appendTo(".news ul");
+            });
+            $.each(data.slide, function (i,val) {
+                var $slideDiv = $(template("swiperTemp",val));
+                $slideDiv.appendTo(".swiper_wp");
+            });
+            $.each(data.hot, function (i,val) {
+                var $hotDiv = $(template("hotTemp",val));
+                $hotDiv.appendTo(".hot_list ul");
+            });
 
+            $(".hot_list li a").hover(function () {
+                $(this).children("h3").css({
+                    color: "#eb0127",
+                    transition: "ease-in-out 0.5s"
+                });
+            },function () {
+                $(this).children("h3").css({
+                    color: "#333",
+                    transition: "ease-in-out 0.5s"
+                });
+            });
+        }
+    })
+});
