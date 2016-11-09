3cca787212a8c554af740831dbbce3f699c6804e
yyh <931147735@qq.com>
2016-11-09 17:01:55 +0800

    作者墙完成上传

--- zuoZhe/js/zuoZhe.js
+++ zuoZhe/js/zuoZhe.js
@@ -1,4 +1,5 @@
 
+// 作者
 $(function () {
     $.ajax({
         url:'json/content.json',
@@ -11,24 +12,18 @@ $(function () {
             var $new = $(".user_content li > .user_main");
             $.each($new,function (i, val) {
                 var $tag = $(val).find(".author_new");
-                console.log($tag.text())
-                if($tag.text() == ""){
-                    // $tag.css("display","none");
-                    console.log($tag);
+                // console.log($tag.text())
+                // .trim() 去掉字符串两边的空格
+                if($tag.text().trim() == ""){
+                    $tag.css("display","none");
+                    // console.log($tag);
                 }
-                // else {
-                //     $(val).find(".test_count span").css({
-                //         border: "1px #d11f3c solid",
-                //         color: "#d11f3c"
-                //     });
-                //     $tag.css("color","#d11f3c")
-                // }
             })
         }
     })
 });
 
-
+// 野玩儿神人
 $(function () {
     $.ajax({
         url:'../json/content.json',
@@ -37,12 +32,69 @@ $(function () {
         success: function (data) {
             $.each(data.shenRen,function (i, value) {
                 $(template('shenRen',value)).appendTo(".user_content ul");
+            });
+            var $new = $(".user_content li > .user_main");
+            $.each($new,function (i, val) {
+                var $tag = $(val).find(".author_new");
+                // console.log($tag.text())
+                // .trim() 去掉字符串两边的空格
+                if($tag.text().trim() == ""){
+                    $tag.css("display","none");
+                    // console.log($tag);
+                }
             })
         }
     })
 });
 
+// 品牌商家
+$(function () {
+    $.ajax({
+        url:'../json/content.json',
+        dataType:'json',
+        method:'get',
+        success: function (data) {
+            $.each(data.pinPai,function (i, value) {
+                $(template('pinPai',value)).appendTo(".user_content ul");
+            });
+            var $new = $(".user_content li > .user_main");
+            $.each($new,function (i, val) {
+                var $tag = $(val).find(".author_new");
+                // console.log($tag.text())
+                // .trim() 去掉字符串两边的空格
+                if($tag.text().trim() == ""){
+                    $tag.css("display","none");
+                    // console.log($tag);
+                }
+            })
+        }
+    })
+});
 
+// 户外媒体
+$(function () {
+    $.ajax({
+        url:'../json/content.json',
+        dataType:'json',
+        method:'get',
+        success: function (data) {
+
+            $.each(data.meiTi,function (i, value) {
+                $(template('meiTi',value)).appendTo(".user_content ul");
+            });
+            var $new = $(".user_content li > .user_main");
+            $.each($new,function (i, val) {
+                var $tag = $(val).find(".author_new");
+                // console.log($tag.text())
+                // .trim() 去掉字符串两边的空格
+                if($tag.text().trim() == ""){
+                    $tag.css("display","none");
+                    // console.log($tag);
+                }
+            })
+        }
+    })
+});
 
 
 
