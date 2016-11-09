3aee072aa1a7e5f5f75a8bf0fe437f8cef483ebf
lujiaolancode <342240955@qq.com>
2016-11-09 10:54:54 +0800

    图标库更新

--- shiPin/js/shiPin.js
+++ shiPin/js/shiPin.js
@@ -1,3 +1,24 @@
 /**
  * Created by lujiaolan on 16/11/8.
  */
+$(function () {
+   $.ajax({
+       url: "json/content.json",
+       success: function (data) {
+           // console.log(data);
+           $.each(data.shenrenzhi, function (i, val) {
+               // console.log(val);
+               $(template("vedioTemp",val)).appendTo(".video_cat:eq(0) .swiper_wrapper");
+           });
+           $.each(data.jixian, function (i, val) {
+               $(template("vedioTemp",val)).appendTo(".video_cat:eq(1) .swiper_wrapper");
+           });
+           $.each(data.zhuangbei, function (i, val) {
+               $(template("vedioTemp",val)).appendTo(".video_cat:eq(2) .swiper_wrapper");
+           });
+           $.each(data.quwei, function (i, val) {
+               $(template("vedioTemp",val)).appendTo(".video_cat:eq(3) .swiper_wrapper");
+           })
+       }
+   })
+});
